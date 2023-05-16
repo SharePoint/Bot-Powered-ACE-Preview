8f934cac28e6378ace5557aedf9ae5293c83f9e9
Ben Tsai <bentsai@microsoft.com>
2023-05-16 12:08:24 -0700

    update nuget packages with schema updates

--- BotBuilder DotNet/libraries/Microsoft.Bot.Schema/SharePoint/SelectMediaAction.cs
+++ BotBuilder DotNet/libraries/Microsoft.Bot.Schema/SharePoint/SelectMediaAction.cs
@@ -18,6 +18,7 @@ namespace Microsoft.Bot.Schema.SharePoint
     {
 #pragma warning disable CA1823 // Avoid unused private fields
 #pragma warning disable CS0414 // The field 'SelectMediaAction.type' is assigned but its value is never used
+        [JsonProperty(PropertyName = "type")]
         private string type = "VivaAction.SelectMedia";
 #pragma warning restore CS0414 // The field 'SelectMediaAction.type' is assigned but its value is never used
 #pragma warning restore CA1823 // Avoid unused private fields
